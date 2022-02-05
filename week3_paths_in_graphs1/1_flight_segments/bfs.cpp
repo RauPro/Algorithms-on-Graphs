6ce8f9ef5fbec04acd29abb2c943cb2c01f9b75e
RauPro <00012119@uca.edu.sv>
2022-02-05 13:03:21 -0600

    Add week 3

--- week3_paths_in_graphs1/1_flight_segments/bfs.cpp
+++ week3_paths_in_graphs1/1_flight_segments/bfs.cpp
@@ -4,10 +4,20 @@
 
 using std::vector;
 using std::queue;
+using namespace std;
+
+vector<int> p;
+const int INF = 1e9;
+int aws = 0;
+void printPath(int u){
+  if (p[u] == -1) {return;}
+  printPath(p[u]);
+  aws++;
+}
 
 int distance(vector<vector<int> > &adj, int s, int t) {
   //write your code here
-  return -1;
+  return aws == 0 ? -1 : aws;
 }
 
 int main() {
@@ -23,5 +33,19 @@ int main() {
   int s, t;
   std::cin >> s >> t;
   s--, t--;
+  vector<int> dist(n, INF); dist[s] = 0;
+  queue<int> q; q.push(s);
+  p.assign(n,-1);
+  while (!q.empty()){
+    int u = q.front(); q.pop();
+    if(u == t) break;
+    for (size_t i = 0; i < adj[u].size(); i++){
+      if (dist[adj[u][i]] != INF)continue;
+      dist[adj[u][i]] = dist[u]+1;
+      p[adj[u][i]] = u;
+      q.push(adj[u][i]);
+    }
+  }
+  printPath(t);
   std::cout << distance(adj, s, t);
 }
--- week3_paths_in_graphs1/2_bipartite/bipartite.cpp
+++ week3_paths_in_graphs1/2_bipartite/bipartite.cpp
@@ -1,24 +1,39 @@
-#include <iostream>
-#include <vector>
-#include <queue>
+// Bicoloring
 
-using std::vector;
-using std::queue;
+#include <bits/stdc++.h>
+using namespace std;
 
-int bipartite(vector<vector<int> > &adj) {
-  //write your code here
-  return -1;
-}
+typedef vector<int> vi;
+
+const int INF = 1e9;
 
 int main() {
-  int n, m;
-  std::cin >> n >> m;
-  vector<vector<int> > adj(n, vector<int>());
-  for (int i = 0; i < m; i++) {
-    int x, y;
-    std::cin >> x >> y;
-    adj[x - 1].push_back(y - 1);
-    adj[y - 1].push_back(x - 1);
-  }
-  std::cout << bipartite(adj);
+  int n;
+  cin>>n;
+    vector<vi> AL(n, vi());                      // notice: vi, not vii
+   int l;cin>>l;
+    while (l--) {
+      int a, b; scanf("%d %d", &a, &b);
+      AL[a-1].push_back(b-1);
+      AL[b-1].push_back(a-1);                        // bidirectional
+    }
+    int s = 0;
+    queue<int> q; q.push(s);
+    vi color(n, INF); color[s] = 0;
+    bool isBipartite = true;                     // add a Boolean flag
+    while (!q.empty() && isBipartite) {          // as with original BFS
+      int u = q.front(); q.pop();
+      for (auto &v : AL[u]) {
+        if (color[v] == INF) {                   // don't record distances
+          color[v] = 1-color[u];                 // just record two colors
+          q.push(v);
+        }
+        else if (color[v] == color[u]) {         // u & v have same color
+          isBipartite = false;                   // a coloring conflict :(
+          break;                                 // optional speedup
+        }
+      }
+    }
+    cout<<isBipartite;
+  return 0;
 }
