75cb50e4cdf3829a1a5a6f73b31e0237ed3ba4b7
RauPro <00012119@uca.edu.sv>
2022-02-20 18:49:20 -0600

    Add: week 5 Min Spanning Trees

--- week5_spanning_trees/1_connecting_points/connecting_points.py
+++ week5_spanning_trees/1_connecting_points/connecting_points.py
@@ -1,17 +1,74 @@
-#Uses python3
+# Uses python3
 import sys
 import math
 
-def minimum_distance(x, y):
-    result = 0.
-    #write your code here
-    return result
+
+class UnionFind:  # OOP style
+    def __init__(self, N):
+        self.p = [i for i in range(N)]
+        self.rank = [0 for i in range(N)]
+        self.setSize = [1 for i in range(N)]
+        self.numSets = N
+
+    def findSet(self, i):
+        if (self.p[i] == i):
+            return i
+        else:
+            self.p[i] = self.findSet(self.p[i])
+            return self.p[i]
+
+    def isSameSet(self, i, j):
+        return self.findSet(i) == self.findSet(j)
+
+    def unionSet(self, i, j):
+        if (not self.isSameSet(i, j)):
+            self.numSets -= 1
+            x = self.findSet(i)
+            y = self.findSet(j)
+        # rank is used to keep the tree short
+        if (self.rank[x] > self.rank[y]):
+            self.p[y] = x
+            self.setSize[x] += self.setSize[y]
+        else:
+            self.p[x] = y
+            self.setSize[y] += self.setSize[x]
+            if (self.rank[x] == self.rank[y]):
+                self.rank[y] += 1
+
+    def numDisjointSets(self):
+        return self.numSets
+
+    def sizeOfSet(self, i):
+        return self.setSize[self.findSet(i)]
+
+
+def distance(x, y, x2, y2):
+    return math.sqrt(math.pow((x - x2), 2) + math.pow((y - y2), 2))
 
 
 if __name__ == '__main__':
-    input = sys.stdin.read()
-    data = list(map(int, input.split()))
-    n = data[0]
-    x = data[1::2]
-    y = data[2::2]
-    print("{0:.9f}".format(minimum_distance(x, y)))
+    n = int(input())
+    nodes = [[] for u in range(2)]
+    for i in range(n):
+        x, y = map(int, input().split(" "))
+        nodes[0].append((x, y))
+        nodes[1].append(i)
+
+    EL = [];
+    for p in range(len(nodes[0])):
+        for d in range(p + 1, len(nodes[0])):
+            EL.append((distance(*nodes[0][p], *nodes[0][d]), nodes[1][p], nodes[1][d]))
+
+    EL.sort()
+    mst_cost = 0
+    num_taken = 0
+    UF = UnionFind(n + 1)
+    for i in range(len(EL)):
+        if num_taken == n - 1:
+            break
+        w, u, v = EL[i]
+        if not UF.isSameSet(u, v):
+            num_taken += 1
+            mst_cost += w
+            UF.unionSet(u, v)
+    print("{:.9f}".format(mst_cost))
